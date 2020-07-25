bfe36192f44c4bd6ca70de58e717c5e5a1fbc784
Andrey Tokarew <44374062+andrwtokar@users.noreply.github.com>
2020-07-26 00:55:00 +0300

    Create project at CLion

--- White_belt/ClassRational/Code/main.cpp
+++ White_belt/ClassRational/Code/main.cpp
@@ -3,6 +3,8 @@
 #include <set>
 #include <map>
 #include <vector>
+#include <exception>
+
 using namespace std;
 
 class Rational {
@@ -13,6 +15,9 @@ public:
         GSD = 0;
     }
     Rational (const int& new_numerator, const int& new_denominator) {
+        if (new_denominator == 0) {
+            throw invalid_argument("Invalid argument");
+        }
         numerator = new_numerator;
         denominator = new_denominator;
 
@@ -34,9 +39,16 @@ public:
         return denominator;
     }
     istream& ReadRational(istream& input) {
-        input >> numerator;
+        int new_numerator, new_denominator;
+        input >> new_numerator;
         input.ignore(1);
-        input >> denominator;
+        input >> new_denominator;
+
+        if (new_denominator == 0) {
+            throw invalid_argument("Invalid argument");
+        }
+        numerator = new_numerator;
+        denominator = new_denominator;
 
         DifferenceFromZero();
         Sign();
@@ -96,6 +108,9 @@ Rational operator* (const Rational& lhs, const Rational& rhs) {
     return {numer, denomir};
 }
 Rational operator/ (const Rational& lhs, const Rational& rhs) {
+    if (rhs.Numerator() == 0) {
+        throw domain_error("Division by zero");
+    }
     int numer = lhs.Numerator()*rhs.Denominator();
     int denomir = lhs.Denominator()*rhs.Numerator();
     return {numer, denomir};
@@ -134,10 +149,125 @@ bool operator< (const Rational& lhs, const Rational& rhs) {
     }
 }
 
+
+
+//Another comparison operators.
+
+/*bool operator> (const Rational& lhs, const Rational& rhs) {
+    Rational result = lhs/rhs;
+    return (result.Numerator()/result.Denominator() > 1);
+}
+bool operator!= (const Rational& lhs, const Rational& rhs) {
+    return !(lhs == rhs);
+}
+bool operator<= (const Rational& lhs, const Rational& rhs) {
+    Rational result = lhs/rhs;
+    return (result.Numerator()/result.Denominator() <= 1);
+}
+bool operator>= (const Rational& lhs, const Rational& rhs) {
+    Rational result = lhs/rhs;
+    return (result.Numerator()/result.Denominator() >= 1);
+}*/
+
+
+
+//This main function for check right work of iostream operations.
+
+
 int main() {
-    return 0;
+    Rational a = Rational();
+    Rational b = Rational();
+    char oper;
+
+    try {
+        cin >> a >> oper >> b;
+
+        if (oper == '+') {
+            cout << a + b << endl;
+        } else if (oper == '-') {
+            cout << a - b << endl;
+        } else if (oper == '*') {
+            cout << a*b << endl;
+        } else if (oper == '/') {
+            cout << a/b << endl;
+        }
+
+    } catch (exception& e) {
+        cout << e.what() << endl;
+    }
 }
 
 
+
+//This main function for check right work of throw exception.
+
+/*
+int main() {
+    try {
+        Rational r(1, 0);
+        cout << "Doesn't throw in case of zero denominator" << endl;
+        return 1;
+    } catch (invalid_argument& e) {
+    }
+
+    try {
+        auto x = Rational(1, 2) / Rational(0, 1);
+        cout << "Doesn't throw in case of division by zero" << endl;
+        return 2;
+    } catch (domain_error& e) {
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}*/
+
+
+
+//This main function for check right work comparison operator.
+
+/*
+int main() {
+    {
+        const set<Rational> rs = {{1, 2}, {1, 25}, {3, 4}, {3, 4}, {1, 2}};
+        if (rs.size() != 3) {
+            cout << "Wrong amount of items in the set" << endl << "Size: " << rs.size() << endl;
+            for (auto i: rs) {
+                cout << i << endl;
+            }
+
+            return 1;
+        }
+
+        for (auto i: rs) {
+            cout << "else" << i << endl;
+        }
+
+        vector<Rational> v;
+        for (auto x : rs) {
+            v.push_back(x);
+        }
+        if (v != vector<Rational>{{1, 25}, {1, 2}, {3, 4}}) {
+            cout << "Rationals comparison works incorrectly" << endl;
+            return 2;
+        }
+    }
+
+    {
+        map<Rational, int> count;
+        ++count[{1, 2}];
+        ++count[{1, 2}];
+
+        ++count[{2, 3}];
+
+        if (count.size() != 2) {
+            cout << "Wrong amount of items in the map" << endl;
+            return 3;
+        }
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}
+*/
 
 
